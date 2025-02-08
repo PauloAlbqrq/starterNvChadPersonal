4f91cae6aa8996542024de8d8a4243767901839c
PauloAlbqrq <phximenes38@gmail.com>
2025-02-07 20:44:39 -0400

    formatador python-> black e comform timeout-> 2500

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,12 +3,12 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    python = { "pyink" },
+    python = { "black" },
   },
 
   format_on_save = {
     --   -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 2500,
     lsp_fallback = true,
   },
 }
